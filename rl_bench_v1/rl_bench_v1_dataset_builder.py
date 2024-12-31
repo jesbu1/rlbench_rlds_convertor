70075bfd50981d9dc00393567c92514480406d2e
Jesse Zhang <jessezhang5@gmail.com>
2024-12-31 08:18:27 -0800

    increase num workers and max epsidoes

--- rl_bench_v1/rl_bench_v1_dataset_builder.py
+++ rl_bench_v1/rl_bench_v1_dataset_builder.py
@@ -154,9 +154,9 @@ class RLBenchV1(MultiThreadedDatasetBuilder):
     RELEASE_NOTES = {
         "1.0.0": "Initial release.",
     }
-    N_WORKERS = 5  # number of parallel workers for data conversion
+    N_WORKERS = 10  # number of parallel workers for data conversion
     MAX_PATHS_IN_MEMORY = (
-        50  # number of paths converted & stored in memory before writing to disk
+        100  # number of paths converted & stored in memory before writing to disk
     )
     # -> the higher the faster / more parallel conversion, adjust based on avilable RAM
     # note that one path may yield multiple episodes and adjust accordingly
