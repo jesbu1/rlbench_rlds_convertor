af9c800100f30f96d73ecb6b4067b4b8f2102de4
Jesse Zhang <jessezhang5@gmail.com>
2024-12-31 08:03:23 -0800

    multithreaded version

--- rl_bench_v1/rl_bench_v1_dataset_builder.py
+++ rl_bench_v1/rl_bench_v1_dataset_builder.py
@@ -3,6 +3,7 @@ import pickle
 import h5py
 
 import glob
+from rl_bench_v1.conversion_utils import MultiThreadedDatasetBuilder
 import numpy as np
 import tensorflow as tf
 import tensorflow_datasets as tfds
@@ -29,6 +30,103 @@ exclude_tasks = ["basketball_in_hoop", "change_channel",  "empty_dishwasher", "g
                          "take_shoes_out_of_box"]
 
 
+def _generate_examples(path) -> Iterator[Tuple[str, Any]]:
+    """Generator of examples for each split."""
+    _embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder-large/5")
+
+    def _parse_example(
+        episode_path,
+        which_episode: int,
+        total_episodes: int,
+    ):
+        with open(episode_path + "/variation_descriptions.pkl", "rb") as f:
+            language_instructions = pickle.load(f)
+        language_instruction = np.random.choice(language_instructions)
+        language_embedding = _embed([language_instruction])[0].numpy()
+        with open(episode_path + "/low_dim_obs.pkl", "rb") as f:
+            demo = pickle.load(f)
+
+        gripper_poses = np.array(
+            [
+                get_action_from_obs(demo._observations[i])
+                for i in range(len(demo._observations))
+            ]
+        ).astype(np.float32)
+
+        episode = []
+        prev_action = gripper_poses[0]
+        # - 1 offset because we're predicting the next action
+        # load the images from an h5 file
+        with h5py.File(episode_path + f"/{CAM_NAME}.h5", "r") as images:
+            for i in range(len(gripper_poses) - 1):
+                curr_action = gripper_poses[i + 1]
+                delta_action = curr_action - prev_action
+                episode.append(
+                    {
+                        "observation": {
+                            "image": load_image(images, i),
+                        },
+                        "action": delta_action if DELTA_ACTION else curr_action,
+                        "discount": 1.0,
+                        "is_first": i == 0,
+                        "reward": float(i == (len(gripper_poses) - 2)),
+                        "is_last": i == (len(gripper_poses) - 2),
+                        "is_terminal": i == (len(gripper_poses) - 2),
+                        "language_instruction": language_instruction,
+                        "language_embedding": language_embedding,
+                    }
+                )
+                prev_action = curr_action
+
+        # create output data sample
+        sample = {"steps": episode, "episode_metadata": {"file_path": episode_path}}
+
+        print(
+            f"---------------------Generated {which_episode} episode out of {total_episodes}---------------------"
+        )
+
+        # if you want to skip an example for whatever reason, simply return None
+        return episode_path, sample
+
+    # create list of all examples by recursively finding all subfolders in path with the name variation*
+    variations_paths = glob.glob(
+        f"{path}/*/*variation*", recursive=True
+    )  # Colosseum or RLBench
+    # variations_paths = glob.glob(f"{path}/*/all_variations", recursive=True) # Just RLBench
+
+    if DEBUG:
+        print("---------------------DEBUG MODE-----------------------------")
+        variations_paths = variations_paths[:2]
+
+    # now for each variation* path we load the language descriptions in `variation_descriptions.pkl`
+    # and add them to the example
+    print(
+        f"---------------------Found {len(variations_paths)} variations in {path}--------------------"
+    )
+    examples = []
+    for variation in variations_paths:
+        # load language descriptions
+
+        for episode_path in glob.glob(f"{variation}/episodes/episode*"):
+            if any([excluded_name in episode_path for excluded_name in exclude_tasks]):
+                continue
+            examples.append(
+                (
+                    episode_path,
+                    len(examples),
+                )
+            )
+
+    print(
+        f"-----------------------Will create {len(examples)} trajectories from {path}--------------------------------"
+    )
+    # add len(examples) to each example so we can track progress
+    examples = [(*example, len(examples)) for example in examples]
+
+    for example in examples:
+        yield _parse_example(*example)
+
+
 def load_image(image_h5, i):
     # load a png using PIL
     image_string = image_h5[f"image_{i}"][()]
@@ -49,19 +147,22 @@ def get_action_from_obs(obs):
     return np.concatenate([gripper_pose[:3], actions_euler, gripper_close])
 
 
-class RLBenchV1(tfds.core.GeneratorBasedBuilder):
+class RLBenchV1(MultiThreadedDatasetBuilder):
     """DatasetBuilder for example dataset."""
 
     VERSION = tfds.core.Version("1.0.0")
     RELEASE_NOTES = {
         "1.0.0": "Initial release.",
     }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._embed = hub.load(
-            "https://tfhub.dev/google/universal-sentence-encoder-large/5"
-        )
+    N_WORKERS = 5  # number of parallel workers for data conversion
+    MAX_PATHS_IN_MEMORY = (
+        50  # number of paths converted & stored in memory before writing to disk
+    )
+    # -> the higher the faster / more parallel conversion, adjust based on avilable RAM
+    # note that one path may yield multiple episodes and adjust accordingly
+    PARSE_FCN = (
+        _generate_examples  # handle to parse function from file paths to RLDS episodes
+    )
 
     def _info(self) -> tfds.core.DatasetInfo:
         """Dataset metadata (homepage, citation,...)."""
@@ -142,100 +243,3 @@ class RLBenchV1(tfds.core.GeneratorBasedBuilder):
 
         ret_dict.update({"val": self._generate_examples(path=VAL_PATH)})
         return ret_dict
-
-    def _generate_examples(self, path) -> Iterator[Tuple[str, Any]]:
-        """Generator of examples for each split."""
-
-        def _parse_example(
-            episode_path,
-            which_episode: int,
-            total_episodes: int,
-        ):
-            with open(episode_path + "/variation_descriptions.pkl", "rb") as f:
-                language_instructions = pickle.load(f)
-            language_instruction = np.random.choice(language_instructions)
-            language_embedding = self._embed([language_instruction])[0].numpy()
-            with open(episode_path + "/low_dim_obs.pkl", "rb") as f:
-                demo = pickle.load(f)
-
-            gripper_poses = np.array(
-                [
-                    get_action_from_obs(demo._observations[i])
-                    for i in range(len(demo._observations))
-                ]
-            ).astype(np.float32)
-
-            episode = []
-            prev_action = gripper_poses[0]
-            # - 1 offset because we're predicting the next action
-            # load the images from an h5 file
-            with h5py.File(episode_path + f"/{CAM_NAME}.h5", "r") as images:
-                for i in range(len(gripper_poses) - 1):
-                    curr_action = gripper_poses[i + 1]
-                    delta_action = curr_action - prev_action
-                    episode.append(
-                        {
-                            "observation": {
-                                "image": load_image(images, i),
-                            },
-                            "action": delta_action if DELTA_ACTION else curr_action,
-                            "discount": 1.0,
-                            "is_first": i == 0,
-                            "reward": float(i == (len(gripper_poses) - 2)),
-                            "is_last": i == (len(gripper_poses) - 2),
-                            "is_terminal": i == (len(gripper_poses) - 2),
-                            "language_instruction": language_instruction,
-                            "language_embedding": language_embedding,
-                        }
-                    )
-                    prev_action = curr_action
-
-            # create output data sample
-            sample = {"steps": episode, "episode_metadata": {"file_path": episode_path}}
-
-            print(
-                f"---------------------Generated {which_episode} episode out of {total_episodes}---------------------"
-            )
-
-            # if you want to skip an example for whatever reason, simply return None
-            return episode_path, sample
-
-        # create list of all examples by recursively finding all subfolders in path with the name variation*
-        variations_paths = glob.glob(f"{path}/*/*variation*", recursive=True) #Colosseum or RLBench
-        #variations_paths = glob.glob(f"{path}/*/all_variations", recursive=True) # Just RLBench
-
-        if DEBUG:
-            print("---------------------DEBUG MODE-----------------------------")
-            variations_paths = variations_paths[:2]
-
-        # now for each variation* path we load the language descriptions in `variation_descriptions.pkl`
-        # and add them to the example
-        print(
-            f"---------------------Found {len(variations_paths)} variations in {path}--------------------"
-        )
-        examples = []
-        for variation in variations_paths:
-            # load language descriptions
-
-            for episode_path in glob.glob(f"{variation}/episodes/episode*"):
-                if any([excluded_name in episode_path for excluded_name in exclude_tasks]):
-                    continue
-                examples.append(
-                    (
-                        episode_path,
-                        len(examples),
-                    )
-                )
-
-        print(
-            f"-----------------------Will create {len(examples)} trajectories from {path}--------------------------------"
-        )
-        # add len(examples) to each example so we can track progress
-        examples = [(*example, len(examples)) for example in examples]
-
-        ### SINGLE THREADED CODE
-        #for example in examples:
-        #    yield _parse_example(*example)
-        #### for multiprocessed
-        beam = tfds.core.lazy_imports.apache_beam
-        return beam.Create(examples) | beam.MapTuple(_parse_example)
